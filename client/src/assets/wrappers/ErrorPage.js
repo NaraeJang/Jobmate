69efa0499dbdd2ef2a671373005667126e98dcf7
NaraeJang <j.naaaarae@gmail.com>
2024-01-31 14:47:03 -0500

    complete Error page CSS

---
+++ client/src/assets/wrappers/ErrorPage.js
@@ -0,0 +1,48 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.main`
+  width: 100vw;
+  .container {
+    height: 100vh;
+
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+
+    text-align: center;
+  }
+  h2 {
+    font-size: clamp(8rem, 15vw, 15rem);
+  }
+
+  h3 {
+    font-size: clamp(2rem, 3.375vw, 3.375rem);
+  }
+
+  p {
+    padding: 0 10vw;
+    color: var(--grey-700);
+  }
+
+  @media (min-width: 992px) {
+    .container {
+      flex-direction: row;
+      text-align: start;
+      gap: 1.5rem;
+    }
+
+    h2 {
+    }
+
+    p {
+      padding: 0;
+    }
+
+    .box-text > * {
+      margin-bottom: 0.5rem;
+    }
+  }
+`;
+
+export default Wrapper;
--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,15 +1,18 @@
 import { Link } from 'react-router-dom';
+import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
   return (
-    <div className="container">
-      <h2>404</h2>
-      <div>
-        <h3>Page Not Found</h3>
-        <p>We can't seem to find the page you are looking for</p>
-        <Link to="/">Back Home</Link>
+    <Wrapper>
+      <div className="container">
+        <h2>404</h2>
+        <div className="box-text">
+          <h3>Page Not Found</h3>
+          <p>We can't seem to find the page you are looking for</p>
+          <Link to="/">Back Home</Link>
+        </div>
       </div>
-    </div>
+    </Wrapper>
   );
 };
 export default Error;
