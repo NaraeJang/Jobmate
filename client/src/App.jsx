6fcc0e55eca85fa7fd871016551400a23305870a
NaraeJang <j.naaaarae@gmail.com>
2024-01-31 13:30:37 -0500

    setup router & create pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,11 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+
+const router = createBrowserRouter([
+  { path: '/', element: <h1>home</h1> },
+  { path: '/about', element: <h1>about</h1> },
+]);
+
 const App = () => {
-  return <div>Jobmate App</div>;
+  return <RouterProvider router={router} />;
 };
 export default App;
