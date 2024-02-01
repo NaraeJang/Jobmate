c72583fb7625989aaafb9b5255be57a28897bcfc
NaraeJang <j.naaaarae@gmail.com>
2024-02-01 17:11:11 -0500

    update navbar landingpage

--- client/src/assets/wrappers/NavbarMobile.js
+++ client/src/assets/wrappers/NavbarMobile.js
@@ -20,9 +20,6 @@ const Wrapper = styled.nav`
     align-items: center;
     justify-content: space-between;
     height: 3.5rem;
-
-    /* background: lightblue; */
-    /* border-bottom: 1px solid var(--grey-200); */
   }
 
   .nav-toggle {
@@ -77,41 +74,6 @@ const Wrapper = styled.nav`
   li:last-child {
     margin-bottom: 1.5rem;
   }
-
-  /* div {
-    height: 4.625rem;
-    width: 100vw;
-    width: 100svw;
-
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-    padding-left: 5vw;
-    padding-right: 5vw;
-    border-bottom: 1px solid #e5e5e5;
-  }
-
-  a:first-child {
-    display: flex;
-    align-items: center;
-  }
-
-  ul {
-    display: flex;
-    align-items: center;
-    gap: 2rem;
-    text-align: center;
-  }
-
-  a:not(.btn) {
-    color: var(--grey-700);
-    text-decoration: none;
-  }
-
-  a:hover:not(.btn) {
-    color: var(--primary-400);
-    font-weight: var(--font-weight-semibold);
-  } */
 
   @media (min-width: 769px) {
     display: none;
