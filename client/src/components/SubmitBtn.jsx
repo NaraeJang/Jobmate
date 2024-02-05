e20edf96ced69fd8dfa810485a02b3c9f846aac4
NaraeJang <j.naaaarae@gmail.com>
2024-02-05 17:04:16 -0500

    Structure / Setup AddJob

--- client/src/components/SubmitBtn.jsx
+++ client/src/components/SubmitBtn.jsx
@@ -3,6 +3,7 @@ import { useNavigation } from 'react-router-dom';
 const SubmitBtn = ({ text, submitting }) => {
   const navigation = useNavigation();
   const isSubmitting = navigation.state === 'submitting';
+
   return (
     <button type="button" className="btn btn-primary btn-block">
       {isSubmitting ? submitting : text}
--- client/src/pages/AddJob.jsx
+++ client/src/pages/AddJob.jsx
@@ -1,6 +1,7 @@
 import { Form } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/AddJob';
-import { FormRow } from '../components';
+import { FormRow, FormRowSelect, SubmitBtn } from '../components';
+import { JOB_STATUS, JOB_TYPE } from '../../utils/constants';
 
 const AddJob = () => {
   return (
@@ -10,6 +11,19 @@ const AddJob = () => {
           <FormRow type="text" name="position" />
           <FormRow type="text" name="company" />
           <FormRow type="text" name="city" />
+          <FormRowSelect
+            labelText="Job Status"
+            name="jobStatus"
+            defaultValue={JOB_STATUS.PENDING}
+            list={Object.values(JOB_STATUS)}
+          />
+          <FormRowSelect
+            labelText="job type"
+            name="jobType"
+            defaultValue={JOB_TYPE.FULL_TIME}
+            list={Object.values(JOB_TYPE)}
+          />
+          <SubmitBtn text="Add Applied Job" submitting="Creating..." />
         </div>
       </Form>
     </Wrapper>
