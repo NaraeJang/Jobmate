c5583e89b548154597a7214df9d791cc70061dd4
NaraeJang <j.naaaarae@gmail.com>
2024-02-07 15:11:39 -0500

    JS/ add if for 404 status error

--- client/src/pages/Error.jsx
+++ client/src/pages/Error.jsx
@@ -1,14 +1,31 @@
-import { Link } from 'react-router-dom';
+import { Link, useRouteError } from 'react-router-dom';
 import Wrapper from '../assets/wrappers/ErrorPage';
 
 const Error = () => {
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div className="container">
+          <h2>404</h2>
+          <div className="box-text">
+            <h3>Page Not Found</h3>
+            <p>We can't seem to find the page you are looking for</p>
+            <Link to="/dashboard">Back Home</Link>
+          </div>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
     <Wrapper>
       <div className="container">
-        <h2>404</h2>
+        <h2 style={{ fontSize: `8rem` }}>Oops!</h2>
         <div className="box-text">
-          <h3>Page Not Found</h3>
-          <p>We can't seem to find the page you are looking for</p>
+          <h3 style={{ fontSize: `1.5rem` }}>Something went wrong</h3>
+          <p>Please, try again later.</p>
           <Link to="/">Back Home</Link>
         </div>
       </div>
