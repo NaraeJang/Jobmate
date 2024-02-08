2bb1e42bf7134c0bf6baa3a23898897d2104ce93
NaraeJang <j.naaaarae@gmail.com>
2024-02-07 19:52:52 -0500

    Server/ json middleware

--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 import express from 'express';
 const app = express();
 
+app.use(express.json()); // built-in middleware in order to send and receive json from server and front-end.
+
 app.get('/', (req, res) => {
-  res.send('Hello world...');
+  //   res.send('Hello world...');
+
+  console.log(req);
+
+  res.json({ message: 'Data received', data: req.body });
 });
 
 app.listen(5100, () => {
