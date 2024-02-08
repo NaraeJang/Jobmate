882754864f1791e17dc3ad017a0467b3cff995e3
NaraeJang <j.naaaarae@gmail.com>
2024-02-07 20:22:38 -0500

    Server / Morgan and Dotenv

--- server.js
+++ server.js
@@ -1,9 +1,20 @@
+import * as dotenv from 'dotenv';
+dotenv.config(); // if you don't place it on the top, it sometimes causes errors.
 import express from 'express';
 const app = express();
+import morgan from 'morgan'; // Provides log for our request.
+
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 
 app.use(express.json()); // built-in middleware in order to send and receive json from server and front-end.
 
 app.get('/', (req, res) => {
+  res.send('Hello world');
+});
+
+app.post('/', (req, res) => {
   //   res.send('Hello world...');
 
   console.log(req);
@@ -11,6 +22,8 @@ app.get('/', (req, res) => {
   res.json({ message: 'Data received', data: req.body });
 });
 
-app.listen(5100, () => {
-  console.log('server running...');
+const port = process.env.PORT || 5100;
+
+app.listen(port, () => {
+  console.log(`Server is listening on PORT ${port}...`);
 });
