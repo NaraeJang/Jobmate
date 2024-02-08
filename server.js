020388852a0ac782802bfd497e171dd44abc36d0
NaraeJang <j.naaaarae@gmail.com>
2024-02-08 10:50:14 -0500

    Server/ Testing CRUD

--- server.js
+++ server.js
@@ -52,6 +52,7 @@ app.post('/api/v1/jobs', (req, res) => {
 // GET SINGLE JOB
 app.get('/api/v1/jobs/:id', (req, res) => {
   const { id } = req.params;
+
   const job = jobs.find((job) => {
     return job.id === id;
   });
@@ -65,6 +66,54 @@ app.get('/api/v1/jobs/:id', (req, res) => {
   res.status(200).json({ job });
 });
 
+// EDIT JOB
+app.patch('/api/v1/jobs/:id', (req, res) => {
+  const { company, position } = req.body;
+  if (!company || !position) {
+    return res.status(404).json({ msg: `Please provide company and position` });
+  }
+
+  const { id } = req.params;
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  job.company = company;
+  job.position = position;
+
+  res.status(200).json({ msg: `job modified`, job });
+});
+
+// DELETE JOB
+app.delete('/api/v1/jobs/:id', (req, res) => {
+  const { id } = req.params;
+
+  const job = jobs.find((job) => job.id === id);
+
+  if (!job) {
+    return res.status(404).json({ msg: `no job with id ${id}` });
+  }
+
+  const newJobs = jobs.filter((job) => job.id !== id);
+  jobs = newJobs;
+
+  res.status(200).json({ msg: `${id} job was deleted successfully.` });
+});
+
+// CUSTOMIZED NOT FOUND ERROR ROUTE.
+// we already have default not found error route that was provided by express. however, we can customize it.
+app.use('*', (req, res) => {
+  res.status(404).json({ msg: `not found` });
+}); // * means we are going to apply this to all request.
+
+app.use((err, req, res, next) => {
+  console.log(err);
+  res.status(500).json({ msg: `something went wrong...` });
+});
+
+// PORT
 const port = process.env.PORT || 5100;
 
 app.listen(port, () => {
