c9d929cc15166da249e0d268238a61b7f9031b39
NaraeJang <j.naaaarae@gmail.com>
2024-02-08 20:05:21 -0500

    Mongoose / set up UserSchema

---
+++ models/UserModel.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const UserSchema = await new mongoose.Schema({
+  name: String,
+  lastName: {
+    type: String,
+    default: 'type your last name here',
+  },
+  password: String,
+  city: {
+    type: String,
+    default: 'my city',
+  },
+  role: {
+    type: String,
+    enum: ['user', 'admin'],
+    default: 'user',
+  },
+});
+
+export default mongoose.model('user', UserSchema);
