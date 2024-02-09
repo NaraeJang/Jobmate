83a748e45870665851bf78bd66b57d4c4403d052
NaraeJang <j.naaaarae@gmail.com>
2024-02-08 21:16:05 -0500

    hash password

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,7 +1,13 @@
 import { StatusCodes } from 'http-status-codes';
 import User from '../models/UserModel.js';
+import bcrypt from 'bcryptjs';
 
 export const register = async (req, res) => {
+  // a random value is added to the password before hashing
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(req.body.password, salt);
+  req.body.password = hashedPassword;
+
   const isFirstAccount = (await User.countDocuments()) === 0;
   req.body.role = isFirstAccount ? 'admin' : 'user';
 
