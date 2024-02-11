5427e79437a45cf4c9a103f3d6d17dd4bbec1af9
NaraeJang <j.naaaarae@gmail.com>
2024-02-10 22:06:38 -0500

    Add User to Job Routes

--- controllers/jobController.js
+++ controllers/jobController.js
@@ -4,14 +4,14 @@ import { NotFoundError } from '../errors/customErrors.js';
 
 // GET ALL JOBS
 export const getAllJobs = async (req, res) => {
-  console.log(req.user);
-  const jobs = await Job.find({});
+  const jobs = await Job.find({ createdBy: req.user.userId });
 
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 // CREATE A JOB
 export const createJob = async (req, res) => {
+  req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
 
   res.status(StatusCodes.CREATED).json({ job });
